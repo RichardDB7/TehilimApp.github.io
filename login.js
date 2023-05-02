0f52a21ad9f63512d3a8540d5f28ac166d83fb84
Ricardo <ricardodiazbatalla@gmail.com>
2023-05-01 21:53:39 -0300

    refactor

--- login.js
+++ login.js
@@ -5,8 +5,9 @@ var user= document.getElementById("usuario").value;
 var pass= document.getElementById("pass").value;
 
 
-if (!localStorage.getItem('token')) {window.location.href = '/index.html';} 
-if(user=="juan" && pass=="1234"){window.location="app.html";}
+
+if(user=="Juan" && pass=="1234"){window.location="app.html";}
+if(user=="Romi" && pass=="1234"){window.location="app.html";}
 
 
 }
