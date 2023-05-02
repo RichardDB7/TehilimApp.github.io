8b0813042c7ff4307cd9db653d8abd137678c0ec
Ricardo <ricardodiazbatalla@gmail.com>
2023-05-01 23:04:58 -0300

    add condition

--- login.js
+++ login.js
@@ -1,14 +1,10 @@
 function loguear()
-{
-      
+{     
 var user= document.getElementById("usuario").value;
 var pass= document.getElementById("pass").value;
 
-
-
 if(user=="Ricardo" && pass=="1234"){window.location="app.html";}
 if(user=="Romi" && pass=="1234"){window.location="app.html";}
-
 
 }
 
